f41f283f75ca874d1f344940e97b55a247e62e47
MJ <meet.jamsutkar@datasciencewizards.ai>
2025-03-16 17:23:04 +0530

    Feat: filtera demo v3

--- public/modules/filterDemo.js
+++ public/modules/filterDemo.js
@@ -1,13 +1,45 @@
 // public/modules/filterDemo.js
 angular.module('filterDemoModule', [])
-// Custom filter to reverse a string
-.filter('reverse', function() {
-  return function(input) {
-    if (!input) return '';
-    return input.split('').reverse().join('');
-  };
-})
 .controller('FilterDemoController', ['$scope', function($scope) {
   console.log("FilterDemoController loaded");
-  $scope.sampleText = "AngularJS is awesome!";
+  $scope.sampleText = "Hello, AngularJS!";
+  $scope.snippetCode = 
+`<!DOCTYPE html>
+<html ng-app="filterDemoApp">
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
+  <script>
+    angular.module('filterDemoApp', [])
+    .filter('reverse', function() {
+      return function(input) {
+        if (!input) return '';
+        return input.split('').reverse().join('');
+      };
+    })
+    .controller('DemoController', ['$scope', function($scope) {
+      $scope.sampleText = "Hello, AngularJS!";
+    }]);
+  </script>
+</head>
+<body ng-controller="DemoController">
+  <div>
+    <p>Original Text: {{ sampleText }}</p>
+    <p>Reversed Text: {{ sampleText | reverse }}</p>
+  </div>
+</body>
+</html>`;
+
+  $scope.updateIframe = function() {
+    var iframe = document.getElementById('liveDemoIframe');
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    iframeDoc.open();
+    iframeDoc.write($scope.snippetCode);
+    iframeDoc.close();
+
+    // Manually bootstrap AngularJS within the iframe
+    var script = iframeDoc.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML = "angular.bootstrap(document, ['filterDemoApp']);";
+    iframeDoc.body.appendChild(script);
+  };
 }]);
