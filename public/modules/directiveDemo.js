100057bd2e35f0b2157a96e440d03e939992a2c0
MJ <meet.jamsutkar@datasciencewizards.ai>
2025-03-16 16:53:09 +0530

    Feat: directives demo v3

--- public/modules/directiveDemo.js
+++ public/modules/directiveDemo.js
@@ -15,4 +15,43 @@ angular.module('directiveDemoModule', [])
 .controller('DirectiveDemoController', ['$scope', function($scope) {
   console.log("DirectiveDemoController loaded");
   $scope.demoText = "Hover over this text to see the custom directive in action!";
+  $scope.snippetCode = 
+`<!DOCTYPE html>
+<html ng-app="directiveDemoApp">
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
+  <script>
+    angular.module('directiveDemoApp', [])
+    .directive('highlightText', function() {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          element.on('mouseenter', function() {
+            element.css('background-color', attrs.highlightText || 'yellow');
+          });
+          element.on('mouseleave', function() {
+            element.css('background-color', 'transparent');
+          });
+        }
+      };
+    })
+    .controller('DemoController', ['$scope', function($scope) {
+      $scope.demoText = "Hover over this text to see the custom directive in action!";
+    }]);
+  </script>
+</head>
+<body ng-controller="DemoController">
+  <p highlight-text="lightblue" class="p-2 border rounded">
+    {{ demoText }}
+  </p>
+</body>
+</html>`;
+
+  $scope.updateIframe = function() {
+    var iframe = document.getElementById('liveDemoIframe');
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    iframeDoc.open();
+    iframeDoc.write($scope.snippetCode);
+    iframeDoc.close();
+  };
 }]);
